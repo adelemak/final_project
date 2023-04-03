4acc6950d7f9261732888c9934b7f77613551c7d
adelemak <113456323+adelemak@users.noreply.github.com>
2023-04-03 20:17:52 +0300

    Update nick_quizz.py

--- nick_quizz.py
+++ nick_quizz.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.title('Пройди тест и узнай, какой ты мультик Nickelodeon')
+st.title('Пройди тест и узнай, какой ты мультик :orange[Nickelodeon]:tada:')
+
+st.markdown("Привет! Это наш :orange[**проект**]. Пройди тест, будет весело!:star-struck:")
