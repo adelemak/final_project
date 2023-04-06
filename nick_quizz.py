c36bc02c2bf72db9fc5951ff40c8199dc6f2c872
ddariath <113455281+ddariath@users.noreply.github.com>
2023-04-06 15:04:57 +0300

    Update nick_quizz.py

--- nick_quizz.py
+++ nick_quizz.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title('Пройди тест и узнай, какой ты мультик :blue[Nickelodeon]:tada:')
 
 st.markdown("Привет! Это наш :orange[**проект**]. Пройди тест, будет весело!:star-struck:")
+st.markdown("Надеюсь это работает:tada:")
