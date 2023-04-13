10a60973265e728634a6c3f606a2f89aab158f29
adelemak <113456323+adelemak@users.noreply.github.com>
2023-04-13 19:28:54 +0300

    Update nick_quizz.py

--- nick_quizz.py
+++ nick_quizz.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Пройди тест и узнай, какой ты мультик :orange[Nickelodeon]:tada:')
 
-st.markdown("Привет! Это наш :orange[**проект**]. Пройди тест, будет весело!:star-struck:")
+st.markdown("Привет! Это наш :blue[**проект**]. Пройди тест, будет весело!:star-struck:")
 
 # словарь для подсчета очков результата
 dict_fin = {
