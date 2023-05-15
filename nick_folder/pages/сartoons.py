0cad369f41365938e8196f91c45362104f68c8cf
ddariath <113455281+ddariath@users.noreply.github.com>
2023-05-15 16:21:51 +0300

    Update сartoons.py
    добавлена функция подсчета результата

--- nick_folder/pages/сartoons.py
+++ nick_folder/pages/сartoons.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # словарь для подсчета очков результата
 dict_fin = {
-             'ГБ': 0, 'КП': 0, 'КБ': 0, "ОУЭД": 0, "ДП": 0, "ЭА": 0, "ДН": 0, "ПИМ": 0, "РИК": 0
+             'ГБ': 0, 'КБ': 0, "ДП": 0, "ЭА": 0, "ДН": 0
             }
 
 def question(amount):  # получает на вход количество вариантов ответа для вопросов: 5
@@ -17,7 +17,6 @@ def question(amount):  # получает на вход количество в�
     for quest_num in range (1, 9):
         que = data['answer'].iloc[[6 * quest_num - 6]]
         que_str = que.to_string(index=False)
-        #for num in range(1, amount + 1):
         
         start = (amount + 1) * quest_num - amount
         end = (amount + 1) * quest_num - 1
@@ -29,7 +28,32 @@ def question(amount):  # получает на вход количество в�
         #user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
         #global dict_fin
         #dict_fin[user_result] = + 1
-
+    return user_answ
+        
+def count_result(question(amount)):
+    #учет ответов пользователя  
+    for answer in  question(amount):
+        user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
+        global dict_fin
+        dict_fin[user_result] = + 1
+    
+    # поиск результата с максимальным значением
+    list_fin = dict_fin.values()
+    res_fin = max(list_fin)
+    res_list = []
+    
+    # поиск ключа по максимальному значению
+    for key, value in dict_fin.items():
+        if res_fin == value:
+            res_list.append(key)
+            
+    # выбор результата при одинаковых значениях
+    if len(res_list) > 1:
+        result = random.choice(res_list)
+    else:
+        result = res_list[0]
+    
+    return result
 
 st.title("Пройди тест и узнай, какой ты мультик :orange[Nickelodeon]:tada:")
 
