d5aef627542bfb1a48c3e9bd0e2633c8c9d4adc0
ddariath <113455281+ddariath@users.noreply.github.com>
2023-05-15 15:31:06 +0300

    Update сartoons.py
    исправлена функция для создания теста с 5 вариантами ответов; изменен путь к файлу

--- nick_folder/pages/сartoons.py
+++ nick_folder/pages/сartoons.py
@@ -4,33 +4,32 @@ import streamlit as st
 import pandas as pd
 
 
-#data = pd.read_csv('https://raw.githubusercontent.com/adelemak/final_project/main/nick_data.csv?token=GHSAT0AAAAAACA56NSLR2RH366AS3KZIRLOZBZNQLQ',
- #                  sep=";", on_bad_lines='skip')
 
 # словарь для подсчета очков результата
 dict_fin = {
              'ГБ': 0, 'КП': 0, 'КБ': 0, "ОУЭД": 0, "ДП": 0, "ЭА": 0, "ДН": 0, "ПИМ": 0, "РИК": 0
             }
 
-def question(amount):  # получает на вход количество вариантов ответа для вопросов: 4 или 9
+def question(amount):  # получает на вход количество вариантов ответа для вопросов: 5
 
-    data = pd.read_csv('https://gitlab.com/ddariath/nick_data/-/raw/main/nick_data.csv',
-        sep=";", on_bad_lines='skip')
-    for num in range(1, amount + 1):
+    data = pd.read_csv('../../question_data.csv', sep=";", on_bad_lines='skip')
 
-        que = data['answer'].iloc[[10 * num - 10]]
-        que_str = que.to_string(index=False)
+    for quest_num in range (1, 9):
+        for num in range(1, amount + 1):
 
-        start = 10 * num - 9
-        end = 10 * num - 1
-        answ = data['answer'].loc[start:end].tolist()
+            que = data['answer'].iloc[[6 * num - 6]]
+            que_str = que.to_string(index=False)
 
-        st.markdown(que_str)
-        option = st.radio("скрытый текст вопроса", answ, label_visibility="collapsed")
+            start = 6 * num - 5
+            end = 6 * num - 1
+            answ = data['answer'].loc[start:end].tolist()
 
-        user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
-        global dict_fin
-        dict_fin[user_result] = + 1
+            st.markdown(que_str)
+            option = st.radio("скрытый текст вопроса", answ, label_visibility="collapsed")
+
+            user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
+            global dict_fin
+            dict_fin[user_result] = + 1
 
 
 st.title("Пройди тест и узнай, какой ты мультик :orange[Nickelodeon]:tada:")
@@ -41,4 +40,4 @@ if go_back:
 
 quest_button = st.button(":orange[**Начать тест**]")
 if quest_button:
-    question(9)
+    question(5)
