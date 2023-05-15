65b39f1368057077e2db6c38703ef91780c7ed61
Daria Baranova <dashabaranova04@gmail.com>
2023-05-15 21:49:13 +0300

    объединила файлы cartoons

--- nick_folder/pages/cartoons.py
+++ nick_folder/pages/cartoons.py
@@ -1,33 +1,62 @@
-import streamlit as st
+import os
 from streamlit_extras.switch_page_button import switch_page
 import streamlit as st
 import pandas as pd
 
+
 # словарь для подсчета очков результата
 dict_fin = {
-             'ГБ': 0, 'КП': 0, 'КБ': 0, "ОУЭД": 0, "ДП": 0, "ЭА": 0, "ДН": 0, "ПИМ": 0, "РИК": 0
-            }
+    'ГБ': 0, 'КБ': 0, "ДП": 0, "ЭА": 0, "ДН": 0
+}
+data = pd.read_csv(os.path.join(os.path.dirname(__file__), '../question_data.csv'), sep=";", on_bad_lines='skip')
+
+# data = pd.read_csv('https://gitlab.com/ddariath/nick_data/-/raw/main/question_data.csv', sep=";", on_bad_lines='skip')
 
 def question(amount):  # получает на вход количество вариантов ответа для вопросов: 5
+    global data
+    user_answ = []
+    for quest_num in range(1, 9):
+        que = data['answer'].iloc[[6 * quest_num - 6]]
+        que_str = que.to_string(index=False)
 
-    data = pd.read_csv('../../question_data.csv', sep=";", on_bad_lines='skip')
+        start = (amount + 1) * quest_num - amount
+        end = (amount + 1) * quest_num - 1
+        answ = data['answer'].loc[start:end].tolist()
 
-    for quest_num in range (1, 9):
-        for num in range(1, amount + 1):
+        st.markdown(que_str)
+        option = st.radio("скрытый текст вопроса", answ, label_visibility="collapsed")
+        user_answ.append(option)
+        # user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
+        # global dict_fin
+        # dict_fin[user_result] = + 1
+    return user_answ
 
-            que = data['answer'].iloc[[6 * num - 6]]
-            que_str = que.to_string(index=False)
 
-            start = 6 * num - 5
-            end = 6 * num - 1
-            answ = data['answer'].loc[start:end].tolist()
+def count_result(list_of_answers):
+    global data
+    # учет ответов пользователя
+    for answer in list_of_answers:
+        user_result = data['result'].iloc[data.index[data['answer'] == answer]].values[0]
+        global dict_fin
+        dict_fin[user_result] += 1
 
-            st.markdown(que_str)
-            option = st.radio("скрытый текст вопроса", answ, label_visibility="collapsed")
+    # поиск результата с максимальным значением
+    list_fin = dict_fin.values()
+    res_fin = max(list_fin)
+    res_list = []
 
-            user_result = data['result'].iloc[[data.index[data['answer'] == option]]]
-            global dict_fin
-            dict_fin[user_result] = + 1
+    # поиск ключа по максимальному значению
+    for key, value in dict_fin.items():
+        if res_fin == value:
+            res_list.append(key)
+
+    # выбор результата при одинаковых значениях
+    if len(res_list) > 1:
+        result = random.choice(res_list)
+    else:
+        result = res_list[0]
+
+    return result
 
 
 st.title("Пройди тест и узнай, какой ты мультик :orange[Nickelodeon]:tada:")
@@ -36,18 +65,8 @@ go_back = st.button(":orange[**Вернуться назад**]")
 if go_back:
     switch_page("main code")
 
-quest_button = st.button(":orange[**Начать тест**]")
-if quest_button:
-    question(5)
+count_result(question(5))
 
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("**2. Выберите картинку**")
-    option_3 = st.radio("Выберите картинку", ("1", "2", "3"), label_visibility = "collapsed")
-with col2:
-    st.text("1.")
-    st.image("https://github.com/adelemak/final_project/blob/fd27c51306d3e2a33c7dec0dfef607cf6b2526f1/nick_folder/pictures/Arnold.webp", width=200)
-    st.text("2.")
-    st.image("https://rg.ru/uploads/images/193/20/18/medved.jpg", width=200)
-    st.image("https://media0.giphy.com/media/yoJC2GnSClbPOkV0eA/giphy.gif?cid=ecf05e47ac8960707e6e75ad6b8e0d8c3ca0e061d7774ce1&rid=giphy.gif&ct=g")
+res_button = st.button("Узнать результаты")
+if res_button:
+    switch_page("result")
