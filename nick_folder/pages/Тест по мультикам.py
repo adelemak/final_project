53217a7fc1561de26a7a53c2da3aecb1c535e034
adelemak <113456323+adelemak@users.noreply.github.com>
2023-05-15 14:36:34 +0300

    Update Тест по мультикам.py

--- nick_folder/pages/Тест по мультикам.py
+++ nick_folder/pages/Тест по мультикам.py
@@ -42,15 +42,3 @@ if go_back:
 quest_button = st.button(":orange[**Начать тест**]")
 if quest_button:
     question(9)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.markdown("**2. Выберите картинку**")
-    option_3 = st.radio("Выберите картинку", ("1", "2", "3"), label_visibility = "collapsed")
-with col2:
-    st.text("1.")
-    st.image("https://github.com/adelemak/final_project/blob/fd27c51306d3e2a33c7dec0dfef607cf6b2526f1/nick_folder/pictures/Arnold.webp", width=200)
-    st.text("2.")
-    st.image("https://rg.ru/uploads/images/193/20/18/medved.jpg", width=200)
-    st.image("https://media0.giphy.com/media/yoJC2GnSClbPOkV0eA/giphy.gif?cid=ecf05e47ac8960707e6e75ad6b8e0d8c3ca0e061d7774ce1&rid=giphy.gif&ct=g")
