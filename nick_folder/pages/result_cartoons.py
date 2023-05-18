c3b8899779689b3ba40477096a03846237c5e7e9
adelemak <113456323+adelemak@users.noreply.github.com>
2023-05-18 17:43:16 +0300

    Update result_cartoons.py

--- nick_folder/pages/result_cartoons.py
+++ nick_folder/pages/result_cartoons.py
@@ -13,11 +13,11 @@ def print_result(res):
     i_res = data_res.index[data_res['Result'] == res]
     name_result = data_res['Full_name'].iloc[i_res].values[0]
     st.title(f'Ваш результат - :orange[{name_result}]:')
-    text_result = data_res['Text'].iloc[i_res].values[0]
-    st.subheader(text_result)
     image_result = data_res['Image'].iloc[i_res].values[0]
     image_name = Path(image_result)
     st.image(Image.open(os.path.join(os.path.dirname(__file__), image_name)))
+    text_result = data_res['Text'].iloc[i_res].values[0]
+    st.subheader(text_result)
 
 
 print_result(users_result)
