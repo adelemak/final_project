3a1155cea9bc1d33a17471ee894d8baf5330d390
dontsova.lidia <dontsova.lidia@yandex.ru>
2023-05-18 19:56:28 +0300

    опрос по сериалам

--- nick_folder/pages/result_series.py
+++ nick_folder/pages/result_series.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import pandas as pd
+import os, sys
+from pathlib import Path
+from PIL import Image
+from streamlit_extras.switch_page_button import switch_page
 
-st.title("Hope we will need this")
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from result_cartoons import print_result
+
+users_result = st.session_state['users_result_series']
+
+data_res = pd.read_csv(os.path.join(os.path.dirname(__file__), '../result_series.csv'), sep=";", on_bad_lines='skip')
+
+st.balloons()
+print_result(users_result)
+
+start_test_again = st.button(":purple[**Пройти тест снова**]:rocket:")
+if start_test_again:
+    switch_page("series")
+
+go_to_main = st.button(":purple[**Вернуться на главную**]:taxi:")
+if go_to_main:
+    switch_page("main code")
--- nick_folder/pages/series.py
+++ nick_folder/pages/series.py
@@ -1,7 +1,32 @@
-import streamlit as st
 import os, sys
+from streamlit_extras.switch_page_button import switch_page
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import random
+from pathlib import Path
+
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-from cartoons import question
-question(5)
-st.title('Здесь тест по сериалам')
+from cartoons import question, count_result
+
+# словарь для подсчета очков результата
+dict_fin = {
+    'ДЖ': 0, 'ДЧ': 0, "ЖЗК": 0, "ВВП": 0, "ХМ": 0
+}
+data = pd.read_csv(os.path.join(os.path.dirname(__file__), '../question_series.csv'), sep=";", on_bad_lines='skip')
+
+# код страницы
+st.title("Пройди тест и узнай, какой ты сериал :purple[Disney]:tada:")
+
+go_back = st.button(":purple[**Вернуться назад**]")
+if go_back:
+    switch_page("main code")
+
+list_of_users_answers = question(5)
+
+res_button = st.button(":purple[**Узнать результаты**]")
+if res_button:
+    users_result = count_result(list_of_users_answers)
+    st.session_state['users_result_series'] = users_result
+    switch_page("result_series")
